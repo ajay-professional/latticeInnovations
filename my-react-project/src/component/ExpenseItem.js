5f6382cc643da9dea6381892b6a743bf594732d6
Ajay <ajtheajay03@gmail.com>
2022-11-14 14:33:28 +0530

    React Ist code

---
+++ my-react-project/src/component/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h1>Expense Items</h1>
+            <h2>Food Rs 10</h2>
+            <h3>Petrol Rs 100</h3>
+            <h4>Movies Rs 200</h4>
+        </div>
+    );
+};
+export default ExpenseItem;
