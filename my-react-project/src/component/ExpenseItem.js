08641d71a9900d8b63223e7dc9856001afba484f
Ajay <ajtheajay03@gmail.com>
2022-11-14 15:01:49 +0530

    Dynamic variable react

--- my-react-project/src/component/ExpenseItem.js
+++ my-react-project/src/component/ExpenseItem.js
@@ -1,10 +1,15 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+function ExpenseItem() { 
+    const item1='Food Rs 10';
+    const item2='Petrol Rs 100';
+    const item3='Movies Rs 200';
+    const locationOfExpenditure='Manali'
     return (
-        <div>
+        <div id="myReactJsx">
             <h1>Expense Items</h1>
-            <h2>Food Rs 10</h2>
-            <h3>Petrol Rs 100</h3>
-            <h4>Movies Rs 200</h4>
+            <h2>{item1} {locationOfExpenditure}</h2>
+            <h3>{item2}</h3>
+            <h4>{item3} {locationOfExpenditure}</h4>
         </div>
     );
 };
